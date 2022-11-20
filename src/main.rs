2736ef1137728f5dd7196bcdded9670ae208245c
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-11-20 19:13:40 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ async fn run() {
     env_logger::init();
     let (_, device, queue, query_set) = prep_gpu().await;
 
-    let input_f1: &Vec<f32> = &create_input(2.0f32);
-    let input_f2: &Vec<f32> = &create_input(3.0f32);
+    let input_f1: &Vec<i32> = &create_input(2_i32);
+    let input_f2: &Vec<i32> = &create_input(3_i32);
 
     let input1: &[u8] = bytemuck::cast_slice(input_f1);
     let input2: &[u8] = bytemuck::cast_slice(input_f2);
@@ -131,21 +131,25 @@ async fn run() {
     {
         let now = std::time::Instant::now();
         device.poll(Maintain::Wait);
-        println!("Elapsed: {} ms during poll", now.elapsed().as_millis());
+        println!(
+            "Elapsed: {} ms during poll",
+            now.elapsed().as_nanos() as f64 / 1_000_000.0
+        );
     }
 
     if let Some(Ok(())) = receiver.receive().await {
         let data_raw = &*buf_slice.get_mapped_range();
-        let data: &[f32] = bytemuck::cast_slice(data_raw);
-        println!("data: {:?}", &data);
+        let data: &[i32] = bytemuck::cast_slice(data_raw);
+        println!("data[..10]: {:?}, ({} elements)", &data[..10], data.len());
     } else {
         println!("could not read data");
     }
 }
 
-fn create_input(a: f32) -> Vec<f32> {
-    //vec![2.0f32; 1 << 16 - 1]
-    vec![a; 10]
+fn create_input(a: i32) -> Vec<i32> {
+    //vec![a; 1 << 16 - 1]
+    vec![a; (1 << 16) - 1]
+    //vec![a; 10]
 }
 
 async fn prep_gpu() -> (Features, Device, Queue, Option<QuerySet>) {
@@ -182,12 +186,101 @@ fn prepare_shader(device: &Device) -> ShaderModule {
     let start_instant = Instant::now();
     let cs_module = device.create_shader_module(ShaderModuleDescriptor {
         label: None,
-        source: ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        source: ShaderSource::Wgsl(std::borrow::Cow::Borrowed(include_str!("shader.wgsl"))),
     });
     println!("shader compilation {:?}", start_instant.elapsed());
     cs_module
 }
 
+// CSR without any attached data
+struct Csr {
+    indexes: Vec<u32>,
+    outputs: Vec<u32>,
+}
+impl Csr {
+    // List of lists to compressed sparse row
+    fn from_list_of_lists(ll: Vec<Vec<u32>>) -> Self {
+        let ll: Vec<Vec<u32>> = ll
+            .into_iter()
+            .map(|x| {
+                let mut x = x;
+                x.sort();
+                x.dedup();
+                x
+            })
+            .collect();
+        let mut indexes: Vec<u32> = Vec::new();
+        let mut outputs: Vec<u32> = Vec::new();
+        for mut l in ll {
+            indexes.push(outputs.len() as u32);
+            outputs.append(&mut l);
+        }
+        Self { indexes, outputs }
+    }
+}
+type DenseVector = Vec<u32>;
+
+/// Spmv will do the following:
+/// y = y + Ax, where A is a sparse matrix
+struct SpmvData {
+    csr: Csr,       // outputs
+    x: DenseVector, // delta
+    y: DenseVector, // acc
+}
+impl SpmvData {
+    fn new(csr: Csr, x: DenseVector, y: DenseVector) -> Self {
+        assert_eq!(csr.indexes.len() - 1, x.len());
+        assert_eq!(y.len(), x.len());
+        Self { csr, x, y }
+    }
+    fn new_random() {
+        let density = 0.1;
+        let rand = tinyrand::StdRand::default();
+    }
+}
+
+fn spmv_gpu(mut input: SpmvData) -> SpmvData {
+    todo!()
+}
+
 fn main() {
     pollster::block_on(run());
 }
+
+fn spmv_cpu_reference(mut input: SpmvData) -> SpmvData {
+    for i in 0..input.y.len() {
+        let from_index = input.csr.indexes[i] as usize;
+        let to_index = input.csr.indexes[i + 1] as usize;
+        let delta = input.x[i];
+        for i in input.csr.outputs[from_index..to_index]
+            .iter()
+            .map(|i| *i as usize)
+        {
+            input.y[i] += delta;
+        }
+    }
+    input
+}
+
+fn spmv_cpu_unchecked_indexing(mut input: SpmvData) -> SpmvData {
+    unsafe {
+        for i in 0..input.y.len() {
+            let from_index = *input.csr.indexes.get_unchecked(i) as usize;
+            let to_index = *input.csr.indexes.get_unchecked(i + 1) as usize;
+            let delta = *input.x.get_unchecked(i);
+            for i in input
+                .csr
+                .outputs
+                .get_unchecked(from_index..to_index)
+                .iter()
+                .map(|i| *i as usize)
+            {
+                *input.y.get_unchecked_mut(i) += delta;
+            }
+        }
+    }
+    input
+}
+
+#[cfg(test)]
+mod tests {}
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -2,16 +2,28 @@
 //    data: array<f32>,
 //}
 
-@group(0)
-@binding(42)
-var<storage, read_write> first_array: array<f32>;
 
 @group(0)
 @binding(43)
-var<storage, read_write> second_array: array<f32>;
+var<storage, read_write> second_array: array<atomic<u32>>;
+
+@group(0)
+@binding(42)
+var<storage, read_write> first_array: array<atomic<u32>>;
 
 @compute
 @workgroup_size(1) // does not need to execute in group
 fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-    first_array[global_id.x] = first_array[global_id.x] + second_array[global_id.x];
+    for (var i = 0; i<256; i++) {
+        //first_array[global_id.x] += u32(1);
+        first_array[i] += u32(1);
+        //atomicAdd(&(first_array[i]), u32(1));
+        //atomicAdd(&(first_array[global_id.x]), u32(1));
+        //first_array[global_id.x] = atomicLoad( &(first_array[0]) );
+        //atomicAdd( &(first_array[global_id.x]), u32(1));
+        //first_array[global_id.x] = first_array[global_id.x] + second_array[global_id.x];
+    }
 }
+
+//first_array[0] += i32(1);
+
