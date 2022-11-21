4211c3a3cd4c868c8e3814772eae2944057a9cea
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-11-21 19:40:47 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::hint::black_box;
 use std::time::Instant;
 use wgpu::{
     util::{BufferInitDescriptor, DeviceExt},
@@ -7,149 +8,24 @@ use wgpu::{
     Maintain, MapMode, PipelineLayout, PipelineLayoutDescriptor, QuerySet, QuerySetDescriptor,
     QueryType, Queue, ShaderModule, ShaderModuleDescriptor, ShaderSource, ShaderStages,
 };
-
-async fn run() {
+fn main() {
     env_logger::init();
-    let (_, device, queue, query_set) = prep_gpu().await;
+    let i = 10;
+    let data = black_box(SpmvData::new_random(black_box(i)));
 
-    let input_f1: &Vec<i32> = &create_input(2_i32);
-    let input_f2: &Vec<i32> = &create_input(3_i32);
+    let now = Instant::now();
+    let data_cpu = spmv_cpu_unchecked_indexing(data.clone());
+    let cpu_time = now.elapsed().as_millis();
 
-    let input1: &[u8] = bytemuck::cast_slice(input_f1);
-    let input2: &[u8] = bytemuck::cast_slice(input_f2);
+    let now = Instant::now();
+    let data_gpu = spmv_gpu(data.clone());
+    let gpu_time = now.elapsed().as_millis();
 
-    let input_buffer1: Buffer = {
-        device.create_buffer_init(&BufferInitDescriptor {
-            label: None,
-            contents: input1,
-            usage: BufferUsages::STORAGE | BufferUsages::COPY_DST | BufferUsages::COPY_SRC,
-        })
-    };
-    let input_buffer2: Buffer = {
-        device.create_buffer_init(&BufferInitDescriptor {
-            label: None,
-            contents: input2,
-            usage: BufferUsages::STORAGE | BufferUsages::COPY_DST | BufferUsages::COPY_SRC,
-        })
-    };
-
-    let output_buffer: Buffer = device.create_buffer(&BufferDescriptor {
-        label: None,
-        size: input1.len() as u64,
-        usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
-        mapped_at_creation: false,
-    });
-
-    // Must be initialized.
-    let query_buffer = device.create_buffer_init(&BufferInitDescriptor {
-        label: None,
-        contents: &[0; 16],
-        usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
-    });
-
-    let bind_group_layout = device.create_bind_group_layout(&BindGroupLayoutDescriptor {
-        label: None,
-        entries: &[
-            BindGroupLayoutEntry {
-                binding: 42,
-                visibility: ShaderStages::COMPUTE,
-                ty: BindingType::Buffer {
-                    ty: BufferBindingType::Storage { read_only: false },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            },
-            BindGroupLayoutEntry {
-                binding: 43,
-                visibility: ShaderStages::COMPUTE,
-                ty: BindingType::Buffer {
-                    ty: BufferBindingType::Storage { read_only: false },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            },
-        ],
-    });
-
-    let bind_group = device.create_bind_group(&BindGroupDescriptor {
-        label: None,
-        layout: &bind_group_layout,
-        entries: &[
-            BindGroupEntry {
-                binding: 42,
-                resource: input_buffer1.as_entire_binding(),
-            },
-            BindGroupEntry {
-                binding: 43,
-                resource: input_buffer2.as_entire_binding(),
-            },
-        ],
-    });
-
-    let pipeline: ComputePipeline = {
-        let compute_pipeline_layout: PipelineLayout =
-            device.create_pipeline_layout(&PipelineLayoutDescriptor {
-                label: None,
-                bind_group_layouts: &[&bind_group_layout],
-                push_constant_ranges: &[],
-            });
-        device.create_compute_pipeline(&ComputePipelineDescriptor {
-            label: None,
-            layout: Some(&compute_pipeline_layout),
-            module: &prepare_shader(&device),
-            entry_point: "main",
-        })
-    };
-
-    {
-        let mut encoder = device.create_command_encoder(&Default::default());
-        {
-            let mut cpass = encoder.begin_compute_pass(&Default::default());
-            cpass.set_pipeline(&pipeline);
-            cpass.set_bind_group(0, &bind_group, &[]);
-            cpass.dispatch_workgroups(input_f1.len() as u32, 1, 1);
-        }
-
-        // the operations are done ON the input buffer????
-        encoder.copy_buffer_to_buffer(&input_buffer1, 0, &output_buffer, 0, input1.len() as u64);
-        if let Some(query_set) = &query_set {
-            encoder.resolve_query_set(query_set, 0..2, &query_buffer, 0);
-        }
-        queue.submit(Some(encoder.finish()));
-    }
-
-    let buf_slice = output_buffer.slice(..);
-    let (sender, receiver) = futures_intrusive::channel::shared::oneshot_channel();
-    buf_slice.map_async(MapMode::Read, move |v| sender.send(v).unwrap());
-
-    // Assume that both buffers become available at the same time. A more careful
-    // approach would be to wait for both notifications to be sent.
-    let _query_future = query_buffer.slice(..).map_async(MapMode::Read, |_| ());
-
-    {
-        let now = std::time::Instant::now();
-        device.poll(Maintain::Wait);
-        println!(
-            "Elapsed: {} ms during poll",
-            now.elapsed().as_nanos() as f64 / 1_000_000.0
-        );
-    }
-
-    if let Some(Ok(())) = receiver.receive().await {
-        let data_raw = &*buf_slice.get_mapped_range();
-        let data: &[i32] = bytemuck::cast_slice(data_raw);
-        println!("data[..10]: {:?}, ({} elements)", &data[..10], data.len());
-    } else {
-        println!("could not read data");
-    }
-}
-
-fn create_input(a: i32) -> Vec<i32> {
-    //vec![a; 1 << 16 - 1]
-    vec![a; (1 << 16) - 1]
-    //vec![a; 10]
+    assert_eq!(data_cpu.y, data_gpu.y);
+    println!("Everything works as expected.");
+    println!("Data size: {i}");
+    println!("Elapsed GPU: {gpu_time} ms");
+    println!("Elapsed CPU: {cpu_time} ms");
 }
 
 async fn prep_gpu() -> (Features, Device, Queue, Option<QuerySet>) {
@@ -256,7 +132,11 @@ impl SpmvData {
             })
             .unzip();
         let mut ll: ListOfLists = (0..length).map(|_| Vec::new()).collect();
-        for _ in 0..(((length * length) as f64 * matrix_density) as u32) {
+
+        let connections_to_add = ((length * length) as f64 * matrix_density) as u32;
+        let connections_to_add = length * 16;
+
+        for _ in 0..connections_to_add {
             ll[rng.gen_range(0..length)].push(rng.gen_range(0..length) as u32);
         }
         assert_eq!(ll.len(), length);
@@ -266,7 +146,6 @@ impl SpmvData {
 }
 
 fn spmv_gpu(mut input: SpmvData) -> SpmvData {
-    env_logger::init();
     pollster::block_on(spmv_gpu_i(input))
 }
 
@@ -364,7 +243,6 @@ async fn spmv_gpu_ei(device: &Device, queue: &Queue, mut input: SpmvData) -> Spm
         let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
         cpass.set_pipeline(&compute_pipeline);
         cpass.set_bind_group(0, &bind_group, &[]);
-        cpass.insert_debug_marker("compute spmv");
         cpass.dispatch_workgroups((&input.y).len() as u32, 1, 1);
     }
     encoder.copy_buffer_to_buffer(&y_buffer, 0, &staging_buffer, 0, y_size);
@@ -374,7 +252,9 @@ async fn spmv_gpu_ei(device: &Device, queue: &Queue, mut input: SpmvData) -> Spm
     let (sender, receiver) = futures_intrusive::channel::shared::oneshot_channel();
     buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
 
+    let now = Instant::now();
     device.poll(wgpu::Maintain::Wait);
+    println!("Elapsed {} ms during GPU poll", now.elapsed().as_millis());
 
     input.y = if let Some(Ok(())) = receiver.receive().await {
         // Gets contents of buffer
@@ -399,10 +279,6 @@ async fn spmv_gpu_ei(device: &Device, queue: &Queue, mut input: SpmvData) -> Spm
     input
 }
 
-fn main() {
-    pollster::block_on(run());
-}
-
 fn spmv_cpu_reference(mut input: SpmvData) -> SpmvData {
     for i in 0..input.y.len() {
         let from_index = input.csr.indexes[i] as usize;
@@ -420,6 +296,10 @@ fn spmv_cpu_reference(mut input: SpmvData) -> SpmvData {
 }
 
 fn spmv_cpu_unchecked_indexing(mut input: SpmvData) -> SpmvData {
+    spmv_cpu_unchecked_indexing_in_place(input)
+}
+
+fn spmv_cpu_unchecked_indexing_in_place(mut input: SpmvData) -> SpmvData {
     unsafe {
         for i in 0..input.y.len() {
             let from_index = *input.csr.indexes.get_unchecked(i) as usize;
@@ -445,16 +325,22 @@ mod tests {
     use super::*;
     #[test]
     fn test_unchecked_indexing() {
-        let data = SpmvData::new_random(100);
-        let data1 = spmv_cpu_reference(data.clone());
-        let data2 = spmv_cpu_unchecked_indexing(data.clone());
-        assert_eq!(data1.y, data2.y);
+        for i in [10, 100, 1000] {
+            dbg!(i);
+            let data = SpmvData::new_random(i);
+            let data1 = spmv_cpu_reference(data.clone());
+            let data2 = spmv_cpu_unchecked_indexing(data.clone());
+            assert_eq!(data1.y, data2.y);
+        }
     }
     #[test]
     fn test_gpu() {
-        let data = SpmvData::new_random(100);
-        let data1 = spmv_cpu_reference(data.clone());
-        let data2 = spmv_gpu(data.clone());
-        assert_eq!(data1.y, data2.y);
+        for i in [10, 100, 1000] {
+            dbg!(i);
+            let data = SpmvData::new_random(i);
+            let data1 = spmv_cpu_reference(data.clone());
+            let data2 = spmv_gpu(data.clone());
+            assert_eq!(data1.y, data2.y);
+        }
     }
 }
